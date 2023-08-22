485b3a3fe21ae7a0bb2640782baaca12de6cd576
Andrew Kolisnyk <kolisnyk9@gmail.com>
2023-08-23 00:10:32 +0300

    Initial Commit

---
+++ Destroyed by Ukraine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destroyed by Ukraine
+//
+//  Created by kolisnyk9 on 23/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
